2ab76f490781440fa74cce11b1dbc72d6712b763
Petrovich34 <masyukpetro455@gmail.com>
2024-10-27 13:13:36 +0400

    project 3331

--- 1115.py
+++ 1115.py
@@ -1,1 +1,16 @@
-print ('Hello python')
+def calculator():
+    a = float(input("Введіть перше число: "))
+    b = float(input("Введіть друге число: "))
+    operation = input("Введіть дію (+, -, *, /): ")
+    if operation == '+':
+        print("Результат:", a + b)
+    elif operation == '-':
+        print("Результат:", a - b)
+    elif operation == '*':
+        print("Результат:", a * b)
+    elif operation == '/' and b != 0:
+        print("Результат:", a / b)
+    else:
+        print("Помилка")
+
+calculator()
