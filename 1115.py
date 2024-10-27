b478421c530f41cf5d9ab30968b9996e18c25d56
Petrovich34 <masyukpetro455@gmail.com>
2024-10-27 13:19:50 +0400

    project 3331

--- 1115.py
+++ 1115.py
@@ -1,16 +1,14 @@
-def calculator():
-    a = float(input("Введіть перше число: "))
-    b = float(input("Введіть друге число: "))
-    operation = input("Введіть дію (+, -, *, /): ")
-    if operation == '+':
-        print("Результат:", a + b)
-    elif operation == '-':
-        print("Результат:", a - b)
-    elif operation == '*':
-        print("Результат:", a * b)
-    elif operation == '/' and b != 0:
-        print("Результат:", a / b)
-    else:
-        print("Помилка")
+a = float(input("Введіть перше число: "))
+b = float(input("Введіть друге число: "))
+operation = input("Введіть дію: ")
 
-calculator()
+if operation == '+':
+    print("Результат:", a + b)
+elif operation == '-':
+    print("Результат:", a - b)
+elif operation == '*':
+    print("Результат:", a * b)
+elif operation == '/' and b != 0:
+    print("Результат:", a / b)
+else:
+    print("Помилка: Невідома операція або ділення на нуль!")
